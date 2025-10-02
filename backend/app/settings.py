a80fb1532b99a8dc9ea6c06fa16840d8861d14d6
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2025-10-02 13:17:29 +0800

    Update

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -115,3 +115,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
